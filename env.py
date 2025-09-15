672466b96f050a2d5baf2201ad82c06105ff9f05
christinalicence <christinalicence@gmail.com>
2025-09-15 15:59:19 +0100

    Add .gitignore and env.py

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_CcFdpU3jsPE4@ep-curly-dew-agty1kbb.c-2.eu-central-1.aws.neon.tech/aids_wasp_thump_969382"
+)
+ 
