eb64cfa91768579cf426c00d90d7d8c42eb64571
christinalicence <christinalicence@gmail.com>
2025-09-16 14:13:39 +0100

    protect secret key properly

--- codestar/settings.py
+++ codestar/settings.py
@@ -16,6 +16,9 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+# Get environment variables
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,13 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p4vb^!u52-zvxbm(k(js9^&p1e39&oo1)*z6)wu(g1kwk31g3('
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['practice-my-blog-fd3393450c3b.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
